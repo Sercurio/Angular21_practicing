b2713fc4210538b9e87b8e28cc624b86ad5cc2e8
Sercurio <penalva.louis@gmail.com>
2026-01-13 16:56:45 +0100

    Section2

---
+++ 01. change-detection/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, inject} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+  counter = 0;
+
+  readonly changeDetector = inject(ChangeDetectorRef);
+
+  constructor() {
+    setInterval(() => {
+      this.counter++;
+      console.log('Counter:', this.counter);
+    }, 1000);
+
+    setInterval(() => {
+      this.changeDetector.detectChanges();
+    }, 5000);
+  }
+}
---
+++ 02. behavior-subject-state/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import {CommonModule} from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {interval} from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+  readonly counter$ = interval(1000);
+}
